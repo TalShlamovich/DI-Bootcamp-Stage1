9a3be45aac79473931fbc02b17b365704c9b7499
TalShlamovich <talshlamovich@gmail.com>
2022-07-04 11:04:33 +0300

    did daily challenge bonus in class

--- Week-3/Day-1/DailyChallenge/script.js
+++ Week-3/Day-1/DailyChallenge/script.js
@@ -11,22 +11,45 @@
 // Be careful, each planet has a certain amount of moons. How should you display them?
 // Should you still use an array for the planets ? Or an array of objects ?
 
-let planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
-let planetColor = ["lightGrey", "grey", "green", "red", "brown", "yellow", "lightBlue", "blue"];
+// let planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
+// let planetColor = ["lightGrey", "grey", "green", "red", "brown", "yellow", "lightBlue", "blue"];
 
-for (var i=0; i<planets.length; i++) {
-    var div = document.createElement("div");
-    div.className = "planet";
-    div.innerHTML = planets[i];
-    div.style.backgroundColor = planetColor[i];
-    document.querySelector(".listPlanets").appendChild(div);
-    console.log(div);
+// for (var i=0; i<planets.length; i++) {
+//     var div = document.createElement("div");
+//     div.className = "planet";
+//     div.innerHTML = planets[i];
+//     div.style.backgroundColor = planetColor[i];
+//     document.querySelector(".listPlanets").appendChild(div);
+//     console.log(div);
+// }
+
+let solarSystem = [
+    {name: 'Mercury', moons: 0, color: 'lightgrey', radius: 2439},
+    {name: 'Venus', moons: 0, color: 'grey', radius: 6051},
+    {name: 'Earth', moons: 1, color: 'green', radius: 6378},
+    {name: 'Mars', moons: 2, color: 'red', radius: 3396},
+    {name: 'Jupiter', moons: 79, color: 'brown', radius: 71492},
+    {name: 'Saturn', moons: 82, color: 'yellow', radius: 60268},
+    {name: 'Uranus', moons: 27, color: 'lightblue', radius: 25559},
+    {name: 'Neptune', moons: 14, color: 'blue', radius: 24764},
+]
+
+let root = document.getElementsByClassName('listPlanets')[0];
+
+for (let i=0; i<solarSystem.length; i++) {
+    let planet = document.createElement('div');
+    planet.className = "planet";
+    planet.innerText = solarSystem[i].name;
+    planet.style.backgroundColor = solarSystem[i].color; 
+
+    let x = 0;
+    while (x < solarSystem[i].moons){
+        let moon = document.createElement('div');
+        moon.style.left = (x*100)+'px';
+        moon.className = "moon";
+        planet.appendChild(moon);
+        x++;
+    }
+
+    root.appendChild(planet);
 }
-
-// let planets = [
-//     {"mercury": []},
-//     {"venus": []},
-//     {"earth": ["moon"]},
-//     {"mars": ["deimos", "phobos"]},
-//     {"jupiter": }
-// ]
